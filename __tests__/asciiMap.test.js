4e2112715398a15eb18f17cfd72ffe9ba7211951
jacksonlevine <jack@netbug.us>
2023-02-03 13:06:44 -0800

    test setArray method on custom maps, to set a grid of positions based on a given string

--- __tests__/asciiMap.test.js
+++ __tests__/asciiMap.test.js
@@ -10,4 +10,17 @@ describe('asciiMap object', () => {
   test('it should create an object that extends the Map class', ()=> {
     expect(Object.getPrototypeOf(newMap).toString()).toEqual('[object Map]');
   });
+
+  describe('setArray method', () => {
+    test('it should take in string, height, width, x, and y, and set a grid of positions in the map based on the numbers given', () => {
+      let string =
+      "@#" +
+      "#@";
+      newMap.setArray(string, 2, 2, 0, 0);
+      expect(newMap.get(0+","+0)).toEqual("#");
+      expect(newMap.get(1+","+0)).toEqual("@");
+      expect(newMap.get(0+","+1)).toEqual("@");
+      expect(newMap.get(1+","+1)).toEqual("#");
+    });
+  });
 });
--- src/asciiMap.js
+++ src/asciiMap.js
@@ -2,4 +2,8 @@ export class asciiMap extends Map {
   constructor() {
     super();
   }
+
+  setArray() {
+    
+  }
 }
