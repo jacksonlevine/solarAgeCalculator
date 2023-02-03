92d69e64894e99816aac20492d20eec3a9e3f373
jacksonlevine <jack@netbug.us>
2023-02-03 11:40:28 -0800

    WIP: set up ASCII display that scrolls through a Map of characters

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 window.addEventListener("load", function() {
-  
+  let displaySpot = document.getElementById("mainDisplay");
+  let userY = 0;
+  let uiMap = new Map();
+  window.setInterval(()=> {
+    display(displaySpot, userY);
+  }, 50);
 });
+
+function display(displaySpot, userY) {
+  displaySpot.innerHTML = buildString(userY);
+}
+
+function buildString(userY) {
+  let screenWidth = 40;
+  let screenHeight = 40;
+  let string = "";
+  for(let j = screenHeight + userY; j > 0 + userY; j-=1) {
+    for(let i = 0; i < screenWidth; i+=1) {
+      string += 
+    }
+  }
+}
