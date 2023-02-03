d5978624c5cc6b925fcd3a99b4a36b1f643dfb76
jacksonlevine <jack@netbug.us>
2023-02-03 13:17:11 -0800

    add code for asciiMap.setArray() to pass

--- src/asciiMap.js
+++ src/asciiMap.js
@@ -3,7 +3,13 @@ export class asciiMap extends Map {
     super();
   }
 
-  setArray() {
-    
+  setArray(string, width, height, x, y) {
+    for(let j = height-1; j > -1; j-=1) {
+      for(let i = 0; i < width; i+=1) {
+        let spotX = i+x;
+        let spotY = j+y;
+        this.set(spotX+','+spotY, string[(((height-1)*width)-(j*width))+i]);
+      }
+    }
   }
 }
