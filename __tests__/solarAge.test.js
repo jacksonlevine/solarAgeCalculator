8a96935e42a7f12deaf20cd94afc1c679b58156a
jacksonlevine <jack@netbug.us>
2023-02-03 09:13:17 -0800

    add test to have solarage default to earth if value not provided (R)

--- __tests__/solarAge.test.js
+++ __tests__/solarAge.test.js
@@ -10,4 +10,9 @@ describe('SolarAge class', () => {
   test('it should create an object that takes a planet as an input and stores it as the planet property', () => {
     expect(solar.planet).toEqual("mars");
   });
+
+  test('it should default to earth if the planet argument is not passed', () => {
+    let solarEarth = new SolarAge();
+    expect(solarEarth.planet).toEqual("earth");
+  });
 }); 
