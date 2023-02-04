cbd7f83028614efb0b9660aa1c6492c128522aa2
jacksonlevine <jack@netbug.us>
2023-02-03 16:52:54 -0800

    complete UI and add end of transmission message

--- src/index.js
+++ src/index.js
@@ -43,8 +43,9 @@ function buildString(userY, uiMap) {
   let screenWidth = window.innerWidth / 16;
   let screenHeight = 60;
   let string = "";
+  let leftMargin = 9;
   for (let j = screenHeight + userY; j > 0 + userY; j -= 1) {
-    for (let i = 0; i < screenWidth; i += 1) {
+    for (let i = -leftMargin; i < screenWidth; i += 1) {
       if (uiMap.has(i + "," + j)) {
         if (uiMap.get(i + "," + j) == "@") {
           string += `<span class="red">${uiMap.get(i + "," + j)}</span>`;
@@ -85,7 +86,7 @@ function buildMap(init, age, homePlanet, pastFutureAge) {
     yHead -= 8;
   });
 
-  Array.from("Please press S to scroll down.").forEach((char, index) => {
+  Array.from("Please press the down arrow or S to scroll down.").forEach((char, index) => {
     map.set(index + "," + yHead, char);
   });
 
@@ -187,30 +188,37 @@ function buildMap(init, age, homePlanet, pastFutureAge) {
         yHead -= 7;
       }
     });
+    yHead -=6;
     let date = new Date();
-    let explainString = future ? `From now, ${date.getDay()}, ${date.getMonth()} ${date.getDate()} until you are age ${pastFutureAge} in ${homePlanet} years:` : `Since you were age ${pastFutureAge} in ${homePlanet} years:`;
+    let explainString = future ? `From now, ${date.toLocaleString('en-US', {weekday: 'long', month: 'long', year: 'numeric'})}, until you are age ${pastFutureAge} in ${homePlanet} years:` : `Since you were age ${pastFutureAge} in ${homePlanet} years:`;
     Array.from(explainString).forEach((element, index) => {
       map.set(index+","+yHead, element);
     });
+    yHead -=9;
     solar.planetList.forEach((value, key) => {
       xIndex = 0;
       offset = !offset;
       if (key != homePlanet) {
         solar.planet = key;
-        Array.from(`Your age is ${solar.yearsPassed(0, age, homePlanet).toFixed(2)} years on planet ${key}.`).forEach((char, index) => {
+        Array.from((future ? `${Math.abs(solar.yearsPassed(pastFutureAge, age, homePlanet)).toFixed(2)} years will have passed on ${key}.`: `${solar.yearsPassed(pastFutureAge, age, homePlanet).toFixed(2)} years have passed on ${key}.`)).forEach((char, index) => {
           map.set(((offset) ? 20 : 0) + index + "," + yHead, char);
           xIndex = index + ((offset) ? 20 : 0);
         });
         if (planetArt.has(key)) {
           map.setArray(planetArt.get(key), 6, 6, xIndex + 1, yHead - 3);
         }
-
-        yHead -= 2;
-        Array.from(`One day on ${key} is ${value} hours.`).forEach((char, index) => {
-          map.set(((offset) ? 20 : 0) + index + "," + yHead, char);
-        });
-        yHead -= 7;
+        yHead -= 9;
       }
+    });
+    yHead -= 15;
+    let endString = new String("END OF TRANSMISSION");
+    endString.split(" ").forEach((element) => {
+      for (let i = 0; i < element.length; i += 1) {
+        if (element[i] != " ") {
+          map.setArray(map.bigLetters.get(element[i].toLowerCase()), 7, 7, 0 + (i * 8), yHead);
+        }
+      }
+      yHead -= 8;
     });
 
   } else {
@@ -220,6 +228,16 @@ function buildMap(init, age, homePlanet, pastFutureAge) {
     yHead -= 50;
     Array.from("There's nothing here yet. Submit an age and home planet!").forEach((char, index) => {
       map.set(index + "," + yHead, char);
+    });
+    let endString = new String("END OF TRANSMISSION");
+    yHead -= 15;
+    endString.split(" ").forEach((element) => {
+      for (let i = 0; i < element.length; i += 1) {
+        if (element[i] != " ") {
+          map.setArray(map.bigLetters.get(element[i].toLowerCase()), 7, 7, 0 + (i * 8), yHead);
+        }
+      }
+      yHead -= 8;
     });
   }
 
